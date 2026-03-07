6d6ec6cf5aed528a16d3d6e32b2fd5c6d1a8f819
SerhanToka <serhantokaa@hotmail.com>
2026-03-07 15:30:54 +0300

    feat: log analiz motoru modülerleştirildi ve interaktif CLI arayüzü eklendi

--- Log_Parser.py
+++ Log_Parser.py
@@ -1,4 +1,4 @@
-guvenlik_loglari = [
+veritabani_loglari = [
     {"tarih": "10:00", "ip": "192.168.1.5", "kullanici": "admin", "durum": "success"},
     {"tarih": "10:05", "ip": "10.0.0.1", "kullanici": "root", "durum": "failed"},
     {"tarih": "10:06", "ip": "10.0.0.1", "kullanici": "admin", "durum": "failed"},
@@ -7,23 +7,29 @@ guvenlik_loglari = [
     {"tarih": "10:20", "ip": "192.168.1.5", "kullanici": "admin", "durum": "success"}
 ]
 
-def log_analiz_et(log_listesi):
-    basarisiz_denemeler = 0
-    supheli_ipler = set()
-
+def log_analiz_et(log_listesi, hedef_durum="failed"):
+    eslesen_log_sayisi = 0
+    hedef_ipler = set()
     for log in log_listesi:
-        if log["durum"] == "failed":
-            basarisiz_denemeler += 1
-            print(f"Supheli IP bulundu {log['ip']}")
-            print("~~~~~~~~~")
-            supheli_ipler.add(log["ip"])
+        if log["durum"] == hedef_durum:
+            eslesen_log_sayisi += 1
+            hedef_ipler.add(log["ip"])
 
-    basarisiz_veri = f"Basarisiz deneme sayisi: {basarisiz_denemeler}"
-    supheli_veri = f"Tespit edilen supheli IP'ler: {supheli_ipler}"
+    return eslesen_log_sayisi, hedef_ipler
 
-    return basarisiz_veri, supheli_veri
+basarisiz_loglar, basarisiz_ipler = log_analiz_et(log_listesi=veritabani_loglari, hedef_durum="failed")
+basarili_loglar, basarili_ipler = log_analiz_et(log_listesi=veritabani_loglari, hedef_durum="success")
 
-guvenlik_basarisiz_veri, guvenlik_supheli_veri = log_analiz_et(guvenlik_loglari)
+while True:
+    secim = input("Goruntulemek istediginiz verinin numarasini yaziniz: (1)Basarili Loglar ve IP'ler, (2)Basarisiz Loglar ve IP'ler, (3)Tumu, (q)Cikis")
 
-# print(guvenlik_basarisiz_veri)
-# print(guvenlik_supheli_veri)
+    if secim == "1":
+        print(f"Basarili Log Sayisi: {basarili_loglar}, Basarili IP'ler: {basarili_ipler}")
+    elif secim == "2":
+        print(f"Basarisiz Log Sayisi: {basarisiz_loglar}, Basarisiz IP'ler: {basarisiz_ipler}")
+    elif secim == "3":
+        print(f"Basirili Log Sayisi: {basarili_loglar} | Basarisiz Log Sayisi: {basarisiz_loglar} | Basarili IP'ler: {basarili_ipler} | Basarisiz IP'ler: {basarisiz_ipler} ")
+    elif secim.lower() == "q":
+        break
+    else:
+        print("Yanlis secim yaptiniz, tekrar deneyiniz")
